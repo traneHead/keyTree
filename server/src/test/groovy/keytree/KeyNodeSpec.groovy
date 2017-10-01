3b6275a3f863852b770b46e22179275e02d8febd
David Tiselius <david.tiselius@resurs.se>
2017-10-01 21:40:23 +0200

    Scaffolded testfile for KeyNode addded

---
+++ server/src/test/groovy/keytree/KeyNodeSpec.groovy
@@ -0,0 +1,18 @@
+package keytree
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class KeyNodeSpec extends Specification implements DomainUnitTest<KeyNode> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+        true == false
+    }
+}
